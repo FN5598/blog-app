8f3ddfd1f80747291292a76b4c6a722a38ded186
FN <mcvektor1337@gmail.com>
2025-11-05 11:35:23 +0200

    fixed protected route handling when no cookies present

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import exclamationMark from '/Exclamation-mark.png';
 import closeIcon from '/Close.png'
 import { NavLink, useNavigate } from 'react-router-dom'
+import { HeaderComponent } from "./HeaderComponent";
 
 export function ProtectedRoute({ children }) {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
@@ -19,25 +20,24 @@ export function ProtectedRoute({ children }) {
     }, []);
     if (isAuthenticated === null) return <div>Loading...</div>;
 
-    localStorage.setItem('isAuthenticated', isAuthenticated);
-
     function handleClose() {
         setVisible(false);
-    }
-
-    if(!isAuthenticated) {
         navigate('/');
     }
 
+    localStorage.setItem('isAuthenticated', isAuthenticated);
     return (
         isAuthenticated ? children : (
             visible && (
-                <div className="access-failed-pop-up">
-                    <img src={exclamationMark} className="alert-image" />
-                    <h1>Access Failed</h1>
-                    <p>Currently you are not logged in <NavLink to="/login" >Click here to login</NavLink></p>
-                    <img className="close-icon" src={closeIcon} onClick={handleClose} />
-                </div>)
+                <>
+                    <HeaderComponent />
+                    <div className="access-failed-pop-up">
+                        <img src={exclamationMark} className="alert-image" />
+                        <h1>Access Failed</h1>
+                        <p>Currently you are not logged in <NavLink to="/login" >Click here to login</NavLink></p>
+                        <img className="close-icon" src={closeIcon} onClick={handleClose} />
+                    </div>
+                </>)
         )
     );
 }
