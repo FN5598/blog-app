bc7f61e0392101acaa869ab8559f9bd5a5dee2d4
FN <mcvektor1337@gmail.com>
2025-11-07 15:23:20 +0200

    moved swagger files into docs folder, moved yaml from routes folder into docs

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./src/config/connectDB');
 const cors = require('cors');
-const { swaggerUi, specs } = require('./src/config/swagger');
+const { swaggerUi, specs, swaggerOptions } = require('./src/config/swagger');
 
 const app = express();
 const PORT = 5000;
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 console.log("Swagger specs generated:", !!specs);
 console.log("OpenAPI version:", specs?.openapi)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, swaggerOptions));
 
 app.use("/api/auth", require(path.join(__dirname, "src", "routes", "authRoutes.js")));
 app.use('/api/posts', require(path.join(__dirname, 'src', 'routes', 'postRoutes')));
--- server/src/config/swagger.js
+++ server/src/config/swagger.js
@@ -1,24 +1,56 @@
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+// server/src/config/swagger.js
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+// Load all YAML files
+const swaggerConfig = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
+const schemas = YAML.load(path.join(__dirname, '../docs/schemas.yaml'));
+const authRoutes = YAML.load(path.join(__dirname, '../docs/auth.yaml'));
+
+// Merge everything
+const swaggerDefinition = {
+  ...swaggerConfig,
+  components: {
+    ...swaggerConfig.components,
+    ...schemas.components
+  },
+  paths: {
+    ...swaggerConfig.paths,
+    ...authRoutes.paths
+    // Add other route files here as you create them (posts.yaml, users.yaml, etc.)
+  }
+};
 
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Your Blog API",
-            version: "1.0.0",
-            description: "A complete blogging platform API",
-        },
-        servers: [
-            { url: "http://localhost:5000", description: "Development server" },
-        ]
-    },
-    apis: ["./src/routes/*.js"]
+  definition: swaggerDefinition,
+  apis: [
+    path.join(__dirname, '../routes/*.js') // Still scan route files for any JSDoc comments
+  ],
 };
 
 const specs = swaggerJsdoc(options);
 
+// Optional: Add dark theme
+const swaggerOptions = {
+  customCss: `
+    .swagger-ui { 
+      filter: invert(0.9) hue-rotate(180deg); 
+    }
+    .swagger-ui .scheme-container { 
+      background: inherit; 
+    }
+  `,
+  swaggerOptions: {
+    persistAuthorization: true,
+    docExpansion: 'none',
+    filter: true,
+  }
+};
+
 module.exports = {
-    swaggerUi,
-    specs
+  swaggerUi,
+  specs,
+  swaggerOptions
 };
